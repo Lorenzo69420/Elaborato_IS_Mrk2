09bb08a7afd45b1b711f2f6ac004a4a82d8404a9
Neto <92165314+Lorenzo69420@users.noreply.github.com>
2024-01-15 17:45:27 +0100

    Update Person.java

--- Elabo/src/TestBackEnd/Person.java
+++ Elabo/src/TestBackEnd/Person.java
@@ -1,42 +1,47 @@
 package TestBackEnd;
 
-import java.util.Date;
+import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.sql.Date;
 
 public class Person {
-	private final String IDcode;
-	private final String name;
-	private final String surname;
-	private final String placeBirth;
-	private final Calendar dateBirth;
+	private String IDcode;
+	private String name;
+	private String surname;
+	private String placeBirth;
+	private Calendar dateBirth;
+	private String belonging_category; //TODO: non so se sia il termine corretto
+	private String num_sanitario; //TODO: anche qua
 	private Person tutor = null;
 	
-	public Person(String IDcode, String name, String surname, String placeBirth, Calendar dateBirth) {
+	public Person(String IDcode, String name, String surname, String placeBirth, Calendar dateBirth, String belonging_category, String num_sanitario) {
 		this.IDcode = IDcode;
 		this.name = name;
 		this.surname = surname;
 		this.placeBirth = placeBirth;
 		this.dateBirth = dateBirth;
+		this.belonging_category = belonging_category;
+		this.num_sanitario = num_sanitario;
 	}
 	
-	public void insertTutor(Person tutor) {
+	public void asignTutor(Person tutor) {
 		this.tutor = tutor;
 	}
 	
 	@Override
 	public String toString() {
-		Date birth = dateBirth.getTime();
-		SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+		java.util.Date birth = dateBirth.getTime();
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
 		
 		return "Name: " + name + "\nSurname: " + surname + "\nID code: " + IDcode + "\nPlace of birth: " + placeBirth + "\nDate of birth: " + dateFormatter.format(birth);
 	}
 	
 	public boolean isAdult() {
-		Calendar minBirthAdult = Calendar.getInstance();
+		Calendar currentDate = Calendar.getInstance();
 		
-		minBirthAdult.add(Calendar.YEAR, -18);
+		currentDate.add(Calendar.YEAR, -18);
 		
-		return minBirthAdult.after(this.dateBirth);
+		return currentDate.after(this.dateBirth);
 	}
 }
