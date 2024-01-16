0902dc2218d7cf3dec3ed34c63da763df740650a
Neto <92165314+Lorenzo69420@users.noreply.github.com>
2024-01-16 14:35:18 +0100

    Added insert to person frfr

--- Elabo/src/TestBackEnd/Person.java
+++ Elabo/src/TestBackEnd/Person.java
@@ -1,9 +1,11 @@
 package TestBackEnd;
 
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-//import java.sql.Date;
+import java.sql.Date;
 
 public class Person {
 	private String IDcode;
@@ -12,10 +14,12 @@ public class Person {
 	private String placeBirth;
 	private Calendar dateBirth;
 	private String belonging_category; //TODO: non so se sia il termine corretto
-	private String num_sanitario; //TODO: anche qua
-	private Person tutor = null;
+	private Long num_sanitario; //TODO: anche qua
+	private Person tutor;
 	
-	public Person(String IDcode, String name, String surname, String placeBirth, Calendar dateBirth, String belonging_category, String num_sanitario) {
+	private Connection connection = null;
+	
+	public Person(String IDcode, String name, String surname, String placeBirth, Calendar dateBirth, String belonging_category, Long num_sanitario) throws SQLException {
 		this.IDcode = IDcode;
 		this.name = name;
 		this.surname = surname;
@@ -23,6 +27,14 @@ public class Person {
 		this.dateBirth = dateBirth;
 		this.belonging_category = belonging_category;
 		this.num_sanitario = num_sanitario;
+		
+		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ingegneria", "neto", "fethergay");
+		this.connection = connection;
+	}
+	
+	//TODO
+	public boolean exists() {
+		return false;
 	}
 	
 	public void asignTutor(Person tutor) {
@@ -44,4 +56,24 @@ public class Person {
 		
 		return currentDate.after(this.dateBirth);
 	}
+	
+	
+	
+	public void insert() throws SQLException {
+		var stmt = connection.prepareStatement("INSERT INTO person VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+	    stmt.setString(1, this.IDcode);
+	    stmt.setString(2, this.name);
+	    stmt.setString(3, this.surname);
+	    stmt.setDate(4, new Date(this.dateBirth.getTime().getTime()));
+	    stmt.setString(5, this.placeBirth);
+	    stmt.setLong(6, this.num_sanitario);
+	    stmt.setString(7, this.belonging_category);
+	    stmt.setObject(8, this.tutor);
+	    
+	    System.out.println(stmt);
+
+	    stmt.executeUpdate();
+	}
+
 }
