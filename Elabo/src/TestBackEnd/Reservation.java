aaea0275c2db0159b47c8d9e8be260f7d78cd9cd
Neto <92165314+Lorenzo69420@users.noreply.github.com>
2024-01-16 16:12:00 +0100

    Update Reservation.java

--- Elabo/src/TestBackEnd/Reservation.java
+++ Elabo/src/TestBackEnd/Reservation.java
@@ -1,13 +1,52 @@
 package TestBackEnd;
 
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 public class Reservation {
+	private ReservationState state;
+	private final ReservationType type;
+	private final LocalDateTime date;
+	private final String office;
+	private Person bookedBy;
+	//TODO da spostare
+	private Connection connection;
+	
+	public Reservation(ReservationType type, LocalDateTime date, String office) throws SQLException {
+		this.state = ReservationState.BOOKABLE;
+		this.type = type;
+		this.date = date;
+		this.office = office;
+		this.bookedBy = null;
+		
+		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ingegneria", "neto", "fethergay");
+		this.connection = connection;
+	}
+	
+	public void book(Person person) {
+		this.bookedBy = person;
+		this.state = ReservationState.BOOKED_UP;
+		
+		var statement = connection.prepareStatement("UPDATE reservation SET reservationState = 'BOOKED', person = ? where reservationType = ?)
+	}
+	
+	public void changeState(ReservationState newState) {
+		if(newState == ReservationState.BOOKED_UP) {
+			System.err.println("wrong function\n");
+		}
+		this.state = newState;
+	}
+	
 	public enum ReservationState {
-		BOOCKABLE,
+		BOOKABLE,
 		BOOKED_UP,
 		CONFIRMED,
-		ALREDY_DONE
+		ALREADY_DONE
 	}
 	
 	public enum ReservationType {
@@ -15,22 +54,17 @@ public class Reservation {
 		ISSUANCE
 	}
 	
-	private ReservationState state;
-	private final ReservationType type;
-	private final LocalDateTime date;
-	private final String office;
-	private final Person boockedBy;
-	
-	public Reservation(ReservationType type, LocalDateTime date, String office, Person boockedBy) {
-		this.state = ReservationState.BOOCKABLE;
-		this.type = type;
-		this.date = date;
-		this.office = office;
-		this.boockedBy = boockedBy;
+	public void insert() throws SQLException {
+		var stmt = connection.prepareStatement("INSERT INTO reservation VALUES (?, ?, ?, ?, ?)");
+
+	    stmt.setString(1, this.state.toString());
+	    stmt.setString(2, this.type.toString());
+	    stmt.setTimestamp(3, Timestamp.valueOf(this.date));
+	    stmt.setString(4, this.office);
+	    stmt.setObject(5, this.bookedBy);
+	    
+	    System.out.println(stmt);
+
+	    stmt.executeUpdate();
 	}
-	
-	public void changeState(ReservationState newState) {
-		this.state = newState;
-	}
-	
 }
