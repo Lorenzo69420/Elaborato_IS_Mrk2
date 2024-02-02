1bdebf8df3de9b08a56430ee465b86998fde2a43
ET730 <elia.tonoli@studenti.univr.it>
2024-02-02 16:43:12 +0100

    added tutor fxml

---
+++ AppQuesturaIS/src/controller/AddTutorController.java
@@ -0,0 +1,15 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField IDField;
+
+    @FXML
+    void addTutor(ActionEvent event) {
+
+    }
+
+}
